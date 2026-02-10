fbb11f10026fd07c1091fee7fd2a5a1bcb973213
atharva169 <atharvasinghh72@gmail.com>
2026-02-10 10:08:50 +0530

    Configure ESLint to ignore errors during builds
    Added ESLint configuration to ignore errors during builds.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
